1f7fe5454ef0bcfdf90858f157f93482b07a095c
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-01 19:35:50 -0300

    feat: add basic middlewares (logger and notFound)

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/joseemds/pasta
 
 go 1.22.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
 
-func main(){
-	fmt.Println("salve")
+func logging(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received " + r.Method + " at " + r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func notFound(w http.ResponseWriter, r *http.Request){
+	log.Println("Received req at " + r.RequestURI)
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Use(logging)
+
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/health_check", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "tome")
+	})
+
+
+
+	http.ListenAndServe("localhost:8090", r)
 }
